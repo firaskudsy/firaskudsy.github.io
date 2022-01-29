e5b0466dc84b3044451852d6e357f7dde1e6a882
kudsyf <kudsy.f.project.events@gmail.com>
2022-01-29 10:02:13 -0500

    Updates

---
+++ precache-manifest.9563d978974dd3b573dfa0dec9835bde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b26922ebd62fabdac351df320d09f8b",
+    "url": "/firaskudsy/index.html"
+  },
+  {
+    "revision": "e1262bedbeff73a1e9af",
+    "url": "/firaskudsy/static/css/main.d8002f5c.chunk.css"
+  },
+  {
+    "revision": "4ede8a2730059b021645",
+    "url": "/firaskudsy/static/js/2.7fc4ee3c.chunk.js"
+  },
+  {
+    "revision": "bd81e5c6feaf63e9a711",
+    "url": "/firaskudsy/static/js/3.4183067e.chunk.js"
+  },
+  {
+    "revision": "e1262bedbeff73a1e9af",
+    "url": "/firaskudsy/static/js/main.e6dc28e5.chunk.js"
+  },
+  {
+    "revision": "c4be5c47d6a14e03aadc",
+    "url": "/firaskudsy/static/js/runtime~main.38add1f8.js"
+  }
+]);
