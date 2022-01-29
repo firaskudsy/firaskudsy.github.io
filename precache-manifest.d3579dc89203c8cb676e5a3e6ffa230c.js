75b848a8c0d94e309a06c519243af8355ace6b42
kudsyf <kudsy.f.project.events@gmail.com>
2022-01-29 10:07:33 -0500

    Updates

---
+++ precache-manifest.d3579dc89203c8cb676e5a3e6ffa230c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee0ea66e9b25999acb2dc2dff7b83fb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1262bedbeff73a1e9af",
+    "url": "/static/css/main.d8002f5c.chunk.css"
+  },
+  {
+    "revision": "4ede8a2730059b021645",
+    "url": "/static/js/2.7fc4ee3c.chunk.js"
+  },
+  {
+    "revision": "bd81e5c6feaf63e9a711",
+    "url": "/static/js/3.4183067e.chunk.js"
+  },
+  {
+    "revision": "e1262bedbeff73a1e9af",
+    "url": "/static/js/main.e6dc28e5.chunk.js"
+  },
+  {
+    "revision": "a23df9af8ebe317cbe33",
+    "url": "/static/js/runtime~main.030dd687.js"
+  }
+]);
